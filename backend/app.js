d0e13f8ed4eaf2f55ce63d172933a3bdc239cefd
Meet Khetani <147489256+Meet5703@users.noreply.github.com>
2024-01-06 09:49:18 +0530

    3

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const fs = require("fs");
 const Submission = require("./models/submission");
 const multer = require("multer");
+const cors = require("cors");
 
 require("dotenv").config();
 
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../frontend/public")));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../Frontend/public/"));
 app.use("/videos", express.static(path.join(__dirname, "./upload")));
@@ -40,7 +42,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // Define routes directly within app.js
-app.get("https://film-form.onrender.com", async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     const submissions = await Submission.find();
     res.render("index", { submissions });
@@ -49,7 +51,7 @@ app.get("https://film-form.onrender.com", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
-app.get("https://film-form.onrender.com/payment", (req, res) => {
+app.get("/payment", (req, res) => {
   res.sendFile(path.join(__dirname, "../Frontend/public/payment.html"));
 });
 
@@ -75,65 +77,61 @@ app.get("/download-video/:filename", (req, res) => {
   }
 });
 
-app.post(
-  "https://film-form.onrender.com/submit",
-  upload.single("VideoUpload"),
-  async (req, res) => {
-    console.log("Received POST request at /submit");
-    try {
-      // Retrieve form data
-      const {
-        Name,
-        FatherName,
-        MotherName,
-        Address,
-        email,
-        ActingRole,
-        MobileNumber,
-        WhatsAppNumber,
-      } = req.body;
+app.post("/submit", upload.single("VideoUpload"), async (req, res) => {
+  console.log("Received POST request at /submit");
+  try {
+    // Retrieve form data
+    const {
+      Name,
+      FatherName,
+      MotherName,
+      Address,
+      email,
+      ActingRole,
+      MobileNumber,
+      WhatsAppNumber,
+    } = req.body;
 
-      // Validate if any required fields are missing
-      const requiredFields = [
-        Name,
-        FatherName,
-        MotherName,
-        Address,
-        email,
-        ActingRole,
-        MobileNumber,
-        WhatsAppNumber,
-      ];
-      if (requiredFields.some((field) => !field)) {
-        return res.status(400).send("Missing required fields");
-      }
-
-      // Create a new Submission object
-      const newSubmission = new Submission({
-        Name,
-        FatherName,
-        MotherName,
-        Address,
-        email,
-        ActingRole,
-        MobileNumber,
-        WhatsAppNumber,
-        VideoUpload: req.file ? req.file.path : null,
-      });
-
-      // Save the submission to the database
-      const savedSubmission = await newSubmission.save();
-
-      console.log("Submission saved:", savedSubmission);
-      res.redirect("https://film-form.onrender.com/payment");
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Server Error");
+    // Validate if any required fields are missing
+    const requiredFields = [
+      Name,
+      FatherName,
+      MotherName,
+      Address,
+      email,
+      ActingRole,
+      MobileNumber,
+      WhatsAppNumber,
+    ];
+    if (requiredFields.some((field) => !field)) {
+      return res.status(400).send("Missing required fields");
     }
-  }
-);
 
-app.get("https://film-form.onrender.com/admin", async (req, res) => {
+    // Create a new Submission object
+    const newSubmission = new Submission({
+      Name,
+      FatherName,
+      MotherName,
+      Address,
+      email,
+      ActingRole,
+      MobileNumber,
+      WhatsAppNumber,
+      VideoUpload: req.file ? req.file.path : null,
+    });
+
+    // Save the submission to the database
+    const savedSubmission = await newSubmission.save();
+
+    console.log("Submission saved:", savedSubmission);
+    res.redirect("/payment");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
+
+app.get("/admin", async (req, res) => {
   try {
     // Fetch submissions data (assuming this logic fetches data from the database)
     const submissions = await Submission.find(); // Fetch submissions data from MongoDB or any other database
