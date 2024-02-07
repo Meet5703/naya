e698d37619dcd72e53394baf3de5301a00d56a7b
Meet Khetani <158665731+EIFPE@users.noreply.github.com>
2024-02-07 18:08:02 +0530

    1234

--- backend/app.js
+++ backend/app.js
@@ -168,7 +168,7 @@ app.get("/admin", async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
